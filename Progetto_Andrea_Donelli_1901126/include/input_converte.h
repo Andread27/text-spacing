8449ca5c569591bd7552ff982997ebfd56d97af2
Andrea <168811400+Andread27@users.noreply.github.com>
2024-05-03 11:34:40 +0200

    Delete Progetto_Andrea_Donelli_1901126 directory

--- Progetto_Andrea_Donelli_1901126/include/input_converte.h
+++
@@ -1,9 +0,0 @@
-
-/**
- * Converte un carattere in un intero corrispondente.
- *
- * @param c Il carattere da convertire.
- * @return L'intero corrispondente al carattere.
- */
-int converte(char *c);
-
